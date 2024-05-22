8131d7dce90a62eb3bc56bc8d2257b7f8f1af886
amjadkhaliliah1998 <amjadkhaliliah1998@gmail.com>
2024-05-22 21:12:40 +0000

    Automation and Website

---
+++ ImageCaptioning/image_captioning_app.py
@@ -0,0 +1,25 @@
+import gradio as gr
+import numpy as np
+from PIL import Image
+from transformers import AutoProcessor, BlipForConditionalGeneration
+
+processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+# Suggested code may be subject to a license. Learn more: ~LicenseLog:1050253449.
+def caption_image(image):
+    inputs = processor(images=image, return_tensors="pt")
+    outputs = model.generate(**inputs)
+    caption = processor.decode(outputs[0], skip_special_tokens=True)
+    return caption
+
+
+iface = gr.Interface(
+    fn=caption_image, 
+    inputs=gr.Image(), 
+    outputs="text",
+    title="Image Captioning",
+    description="This is a simple web app for generating captions for images using a trained model."
+)
+
+iface.launch()
